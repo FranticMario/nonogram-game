b74b0b10ba94e96220cc8db9dd95cbb4738aceff
Andrej Dortmann <andrej.dortmann25@gmail.com>
2025-01-31 21:38:39 +0100

    feat: add true dark mode

--- src/app/app.js
+++ src/app/app.js
@@ -1,62 +1,75 @@
-import GameBoardView from "../gameboard/gameboard.js";
-import GameBoardManagmentView from "../gameboard/reset_timer_solution/gameboard-managment.js";
-import GameButtonsView from "../sidebard/game-buttons-container/game-buttons-container.js";
-import GameLevelView from "../sidebard/game-level-container/game-level.js";
-import GameLevelMode from "../sidebard/game-level-mode-container/game-level-mode.js";
-import GameModeSoundTheme from "../sidebard/game-mode-sound-theme/game-mode-sound-theme.js";
-import View from "../utils/view.js";
+import GameBoardView from '../gameboard/gameboard.js'
+import GameBoardManagmentView from '../gameboard/reset_timer_solution/gameboard-managment.js'
+import GameButtonsView from '../sidebard/game-buttons-container/game-buttons-container.js'
+import GameLevelView from '../sidebard/game-level-container/game-level.js'
+import GameLevelMode from '../sidebard/game-level-mode-container/game-level-mode.js'
+import GameModeSoundTheme from '../sidebard/game-mode-sound-theme/game-mode-sound-theme.js'
+import View from '../utils/view.js'
 
 export default class AppView extends View {
-    constructor() {
-        super();
-        this.sidebarContainer = this.createSidebarContainer();
-        this.gameBoardContainer = this.createGameBoardContainer();
+  constructor() {
+    super()
+    this.sidebarContainer = this.createSidebarContainer()
+    this.gameBoardContainer = this.createGameBoardContainer()
+    this.burger = this.createBurgerMenu()
 
-        this.gameLevelContainer = new GameLevelView();
-        this.gameLevelMode = new GameLevelMode();
-        this.nonogramBoard = new GameBoardView(this.gameBoardContainer);
-        this.nonogramGameManagmentContainer = new GameBoardManagmentView(this.nonogramBoard);
-        this.gameButtonsContainer = new GameButtonsView(this.gameLevelContainer, this.gameLevelMode, this.nonogramGameManagmentContainer, this.nonogramBoard);
-        this.gameModeSoundTheme = new GameModeSoundTheme(this.nonogramBoard);
- 
+    this.gameLevelContainer = new GameLevelView()
+    this.gameLevelMode = new GameLevelMode()
+    this.nonogramBoard = new GameBoardView(this.gameBoardContainer)
+    this.nonogramGameManagmentContainer = new GameBoardManagmentView(this.nonogramBoard)
+    this.gameButtonsContainer = new GameButtonsView(
+      this.gameLevelContainer,
+      this.gameLevelMode,
+      this.nonogramGameManagmentContainer,
+      this.nonogramBoard,
+    )
+    this.gameModeSoundTheme = new GameModeSoundTheme(this.nonogramBoard)
 
-        this.gameLevelContainer.register(this.gameLevelMode);
+    this.gameLevelContainer.register(this.gameLevelMode)
 
-        this.gameLevelMode.register(this.nonogramBoard);
-        this.gameLevelMode.register(this.nonogramGameManagmentContainer);
-        this.gameLevelMode.register(this.gameButtonsContainer)
-        this.gameLevelMode.register(this.gameLevelContainer)
+    this.gameLevelMode.register(this.nonogramBoard)
+    this.gameLevelMode.register(this.nonogramGameManagmentContainer)
+    this.gameLevelMode.register(this.gameButtonsContainer)
+    this.gameLevelMode.register(this.gameLevelContainer)
 
+    this.nonogramBoard.register(this.nonogramGameManagmentContainer)
+    this.nonogramBoard.register(this.gameButtonsContainer)
 
-        this.nonogramBoard.register(this.nonogramGameManagmentContainer);
-        this.nonogramBoard.register(this.gameButtonsContainer)
+    this.nonogramGameManagmentContainer.register(this.gameButtonsContainer)
 
+    this.sidebarContainer.append(
+      this.gameLevelContainer.getElement(),
+      this.gameLevelMode.getElement(),
+      this.gameButtonsContainer.getElement(),
+      this.gameModeSoundTheme.getElement(),
+    )
+    this.gameBoardContainer.append(this.nonogramBoard.getElement(), this.nonogramGameManagmentContainer.getElement()),
+      this.burger
+  }
 
-        this.nonogramGameManagmentContainer.register(this.gameButtonsContainer)
+  createSidebarContainer() {
+    const sidebarContainer = this.createElement('div', '', 'sidebar')
+    document.body.append(sidebarContainer)
 
+    return sidebarContainer
+  }
 
+  createGameBoardContainer() {
+    const gameBoardContainer = this.createElement('div', '', 'gameboard')
+    document.body.append(gameBoardContainer)
 
-        this.sidebarContainer.append(this.gameLevelContainer.getElement(), this.gameLevelMode.getElement(), this.gameButtonsContainer.getElement(), this.gameModeSoundTheme.getElement())
-        this.gameBoardContainer.append(this.nonogramBoard.getElement(), this.nonogramGameManagmentContainer.getElement())
-    }
+    return gameBoardContainer
+  }
 
+  createBurgerMenu() {
+    const burger = this.createElement('div', '', 'header__burger')
+    burger.addEventListener('click', e => {
+      e.target.classList.toggle('burger-active')
+      this.sidebarContainer.classList.toggle('open')
+    })
 
-    createSidebarContainer() {
-        const sidebarContainer = this.createElement("div", "", "sidebar")
-        document.body.append(sidebarContainer)
+    document.body.append(burger)
 
-        return sidebarContainer;
-    }
-
-    createGameBoardContainer() {
-        const gameBoardContainer = this.createElement("div", "", "gameboard")
-        document.body.append(gameBoardContainer)
-
-        return gameBoardContainer;
-    }
-
-
-    
-
-
+    return burger
+  }
 }
--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -1,179 +1,172 @@
-import View from "../utils/view.js";
-import puzzles from "../data/puzzles.js";
-import GameButtonsView from "../sidebard/game-buttons-container/game-buttons-container.js";
-import Modal from "../modal/modal.js";
-
+import View from '../utils/view.js'
+import puzzles from '../data/puzzles.js'
+import GameButtonsView from '../sidebard/game-buttons-container/game-buttons-container.js'
+import Modal from '../modal/modal.js'
 
 export default class GameBoardView extends View {
-    constructor(elternElement) {
-        super();
-        this.wrapper = this.createElement('div', "", ["nonograms easy"]);
-        this.relevantNonogram = [];
-        this.relevantMatrixGame = [];
-        this.actions = [];
-        this.audioLeftClick = new Audio("./assets/audio/left-click_1.mp3")
-        this.audioRightClick = new Audio("./assets/audio/right-click.mp3")
-        this.updateNonogram("Maske")
-    }
+  constructor(elternElement) {
+    super()
+    this.wrapper = this.createElement('div', '', ['nonograms easy'])
+    this.relevantNonogram = []
+    this.relevantMatrixGame = []
+    this.actions = []
+    this.audioLeftClick = new Audio('./assets/audio/left-click_1.mp3')
+    this.audioRightClick = new Audio('./assets/audio/right-click.mp3')
+    this.winAudio = new Audio('./assets/audio/win.mp3')
+    this.updateNonogram('Maske')
+  }
 
+  updateNonogram(gameName) {
+    this.wrapper.innerHTML = ''
+    this.appendNonogramView()
+    puzzles.forEach(item => {
+      if (item.name === gameName) {
+        this.appendNonogramHintTop(item)
+        this.appendNonogramHintLeft(item)
+        this.appendNonogramCell(item)
 
-    updateNonogram(gameName) {
-        this.wrapper.innerHTML = ""
-        this.appendNonogramView();
-        puzzles.forEach(item => {
-            if(item.name === gameName) {
-                this.appendNonogramHintTop(item);
-                this.appendNonogramHintLeft(item);
-                this.appendNonogramCell(item);
+        this.relevantMatrixGame = item.matrix
+      }
+    })
+  }
 
-                this.relevantMatrixGame = item.matrix;
-            }
-        })
+  setClassLevelGame(newClass) {
+    this.wrapper.classList.remove(`${this.wrapper.classList[1]}`)
+    this.wrapper.classList.add(newClass)
+  }
 
-    }
+  appendNonogramView() {
+    const nonogramView = this.createElement('div', '', ['nonogram__view'])
+    this.wrapper.append(nonogramView)
+  }
 
-    setClassLevelGame(newClass) {
-        this.wrapper.classList.remove(`${this.wrapper.classList[1]}`)
-        this.wrapper.classList.add(newClass)
-    }
+  appendNonogramHintTop(game) {
+    const nonogramHintTop = this.createElement('div', '', ['nonogram__hint-top'])
 
-    appendNonogramView() {
-        const nonogramView = this.createElement("div", "", ["nonogram__view"])
-        this.wrapper.append(nonogramView);
-    }
-
-    appendNonogramHintTop(game) {
-        const nonogramHintTop = this.createElement("div", "", ["nonogram__hint-top"])
-
-        for(let col = 0; col < game.size; col++) {
-            const hintTopRow = this.createElement("div", "", ["hints__top"]);
-            let consecutiveHints = 0;
-            for (let row = 0; row < game.size; row++) {
-                if (game.matrix[row][col] === 1) {
-                    consecutiveHints++;
-                } else if (consecutiveHints > 0) {
-                    const hint = this.createElement("span", `${consecutiveHints}`, ["hint"]);
-                    hintTopRow.append(hint);
-                    consecutiveHints = 0;
-                }
-            }
-
-            if (consecutiveHints > 0) {
-                const hint = this.createElement("span", `${consecutiveHints}`, ["hint"]);
-                hintTopRow.append(hint);
-                consecutiveHints = 0;
-            }
-
-            nonogramHintTop.append(hintTopRow)
+    for (let col = 0; col < game.size; col++) {
+      const hintTopRow = this.createElement('div', '', ['hints__top'])
+      let consecutiveHints = 0
+      for (let row = 0; row < game.size; row++) {
+        if (game.matrix[row][col] === 1) {
+          consecutiveHints++
+        } else if (consecutiveHints > 0) {
+          const hint = this.createElement('span', `${consecutiveHints}`, ['hint'])
+          hintTopRow.append(hint)
+          consecutiveHints = 0
         }
+      }
 
-        this.wrapper.append(nonogramHintTop)
+      if (consecutiveHints > 0) {
+        const hint = this.createElement('span', `${consecutiveHints}`, ['hint'])
+        hintTopRow.append(hint)
+        consecutiveHints = 0
+      }
 
+      nonogramHintTop.append(hintTopRow)
     }
 
+    this.wrapper.append(nonogramHintTop)
+  }
 
-    appendNonogramHintLeft(game) {
-        const nonogramHintLeft = this.createElement("div", "", ["nonogram__hint-left"])
+  appendNonogramHintLeft(game) {
+    const nonogramHintLeft = this.createElement('div', '', ['nonogram__hint-left'])
 
-        for(let row = 0; row < game.size; row++) {
-            const hintLeftRow = this.createElement("div", "", ["hints__left"]);
+    for (let row = 0; row < game.size; row++) {
+      const hintLeftRow = this.createElement('div', '', ['hints__left'])
 
-            let consecutiveHints = 0;
-            for (let col = 0; col < game.size; col++) {
-                if (game.matrix[row][col] === 1) {
-                    consecutiveHints++;
-                } else if (consecutiveHints > 0) {
-                    const hint = this.createElement("span", `${consecutiveHints}`, ["hint"]);
-                    hintLeftRow.append(hint);
-                    consecutiveHints = 0;
-                }
-            }
-
-            if (consecutiveHints > 0) {
-                const hint = this.createElement("span", `${consecutiveHints}`, ["hint"]);
-                hintLeftRow.append(hint);
-                consecutiveHints = 0;
-            }
-
-            nonogramHintLeft.append(hintLeftRow)
+      let consecutiveHints = 0
+      for (let col = 0; col < game.size; col++) {
+        if (game.matrix[row][col] === 1) {
+          consecutiveHints++
+        } else if (consecutiveHints > 0) {
+          const hint = this.createElement('span', `${consecutiveHints}`, ['hint'])
+          hintLeftRow.append(hint)
+          consecutiveHints = 0
         }
+      }
 
-        this.wrapper.append(nonogramHintLeft)
+      if (consecutiveHints > 0) {
+        const hint = this.createElement('span', `${consecutiveHints}`, ['hint'])
+        hintLeftRow.append(hint)
+        consecutiveHints = 0
+      }
+
+      nonogramHintLeft.append(hintLeftRow)
     }
 
+    this.wrapper.append(nonogramHintLeft)
+  }
 
-    appendNonogramCell(game) {
-        const nonogramGrid = this.createElement ("div", "", ["nonogram__grid"]);
-        nonogramGrid.addEventListener("contextmenu", e => e.preventDefault())
-        for(let i = 0; i < game.size * game.size; i++) {
-            const cell = this.createElement("div", "", ["cell"]);
-            cell.addEventListener("mousedown", (e) => this.toggleColorCell(e, nonogramGrid, game.matrix, game))
-            nonogramGrid.append(cell);
-        }
-        this.wrapper.append(nonogramGrid);
-        this.relevantNonogram = nonogramGrid
+  appendNonogramCell(game) {
+    const nonogramGrid = this.createElement('div', '', ['nonogram__grid'])
+    nonogramGrid.addEventListener('contextmenu', e => e.preventDefault())
+    for (let i = 0; i < game.size * game.size; i++) {
+      const cell = this.createElement('div', '', ['cell'])
+      cell.addEventListener('mousedown', e => this.toggleColorCell(e, nonogramGrid, game.matrix, game))
+      nonogramGrid.append(cell)
+    }
+    this.wrapper.append(nonogramGrid)
+    this.relevantNonogram = nonogramGrid
+  }
+
+  toggleColorCell(event, nonogramContainer, gameMatrix, game) {
+    const currentTarget = event.target
+
+    if (event.button === 0) {
+      if (currentTarget.classList.contains('cross')) currentTarget.classList.remove('cross')
+      currentTarget.classList.toggle('fill')
+      this.audioLeftClick.play()
+    } else if (event.button === 2) {
+      if (currentTarget.classList.contains('fill')) currentTarget.classList.remove('fill')
+      currentTarget.classList.toggle('cross')
+      this.audioRightClick.play()
     }
 
-    toggleColorCell(event, nonogramContainer, gameMatrix, game) {
-        const currentTarget = event.target
+    this.notifyAll(nonogramContainer, game)
+    this.checkToWin(nonogramContainer, gameMatrix, game)
+  }
 
-        if(event.button === 0) {
-            if(currentTarget.classList.contains("cross")) currentTarget.classList.remove("cross")
-            currentTarget.classList.toggle("fill")
-            this.audioLeftClick.play()
-        } else if (event.button === 2) {
-            if(currentTarget.classList.contains("fill")) currentTarget.classList.remove("fill")
-                currentTarget.classList.toggle("cross")
-            this.audioRightClick.play()
-        }
-
-        this.notifyAll(nonogramContainer, game)
-        this.checkToWin(nonogramContainer, gameMatrix, game);
-
+  checkToWin(currentNonogram, currentGame, game) {
+    const gameManagmentContainer = this.actions[0]
+    const newArr = Array.from(currentNonogram.children).map(item => (item.classList.contains('fill') ? 1 : 0))
+    const currentGameFlat = currentGame.flat()
+    if (JSON.stringify(newArr) === JSON.stringify(currentGameFlat)) {
+      Array.from(currentNonogram.children).map(cell => (cell.style.pointerEvents = 'none'))
+      this.actions[0].stopTimer()
+      const modal = new Modal(game.name, gameManagmentContainer.timer.textContent)
+      document.body.prepend(modal.getElement())
+      this.winAudio.play()
     }
+  }
 
-    checkToWin(currentNonogram, currentGame, game) {
-        const gameManagmentContainer = this.actions[0]
-       const newArr =  Array.from(currentNonogram.children).map(item => item.classList.contains("fill") ? 1 : 0)
-        const currentGameFlat = currentGame.flat();
-        if(JSON.stringify(newArr) === JSON.stringify(currentGameFlat)) {
-            Array.from(currentNonogram.children).map(cell => cell.style.pointerEvents = "none")
-            this.actions[0].stopTimer()
-           const modal = new Modal(game.name, gameManagmentContainer.timer.textContent)
-           document.body.prepend(modal.getElement())
-        }
-    }
+  getRelevantenNonogram() {
+    return this.relevantNonogram.children
+  }
 
-    getRelevantenNonogram() {
-        return this.relevantNonogram.children;
-    }
+  getRelevantenMatrixGame() {
+    return this.relevantMatrixGame
+  }
 
-    getRelevantenMatrixGame() {
-        return this.relevantMatrixGame;
-    }
+  getNonogramCointainer() {
+    return this.relevantNonogram.children
+  }
 
-    getNonogramCointainer() {
-        return this.relevantNonogram.children;
-    }
+  notifyAll(actulyGameboard, game) {
+    return this.actions.forEach(subs => {
+      if (subs instanceof GameButtonsView) {
+        subs.btnDisabled = false
+        subs.createSaveButton(actulyGameboard, game)
+      } else {
+        subs.startTimer()
+      }
+    })
+  }
 
-    notifyAll(actulyGameboard, game) {
-       return this.actions.forEach(subs => {
-            if(subs instanceof GameButtonsView) {
-                subs.btnDisabled = false;
-                subs.createSaveButton(actulyGameboard, game)
+  register(observer) {
+    this.actions.push(observer)
+  }
 
-            } else {
-                subs.startTimer();
-            }
-
-        } )
-    }
-
-    register(observer) {
-        this.actions.push(observer)
-    }
-
-    getElement() {
-        return this.wrapper
-    }
+  getElement() {
+    return this.wrapper
+  }
 }
